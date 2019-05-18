e461b9de5675f6fb0e48a24b4ec099cf477c94cb
Abhishek Verma <abhiverma866@gmail.com>
2019-05-18 13:53:16 +0530

    Commented unused location variable

--- AveragePacketDelay.js
+++ AveragePacketDelay.js
@@ -3,12 +3,12 @@ var send_table = []; // send_table to store <node_id, seq_no, send_time>
 /*save send time function*/
 function saveSendTime(element){
   var exists=0;
-  var location;
+  //var location;
   var i;
   
   for(i=0; i<send_table.length;i++){
     if(send_table[i].node_id == element.node_id){
-      location = i;
+      //location = i;
       exists = 1;
       break;
     }
@@ -37,34 +37,49 @@ function lookupSendTime(element){
   return -1;
 }
 
+
+// function networkLatency(element){
+//   var node_detail;
+//   node_detail = element;
+//   saveSendTime(node_detail);
+//   console.log(send_table);
+//   console.log("\n");
+// }
+
+
+
+//calling sub functions form main 
+function main(){
+  var node_detail;
+  node_detail = {node_id: 1, seq_no: 1, send_time: 78343534};
+  saveSendTime(node_detail);
+  console.log(send_table);
+  console.log("\n");
+  node_detail = {node_id: 2, seq_no: 1, send_time: 78343534};
+  saveSendTime(node_detail);
+  console.log(send_table);
+  console.log("\n");
+  node_detail = {node_id: 2, seq_no: 1, send_time: 7834};
+  saveSendTime(node_detail);
+  console.log(send_table);
+  console.log("\n");
+  node_detail = {node_id: 3, seq_no: 1, send_time: 783454};
+  saveSendTime(node_detail);
+  console.log(send_table);
+  console.log("\n");
+  node_detail = {node_id:2, seq_no:1, send_time:4545};
+  var sendTime;
+  sendTime = lookupSendTime(node_detail);
+  console.log("sendTime = " + sendTime);
+  console.log("\n");
+  console.log(send_table);
+  console.log("\n");
+  
+}
+
+main();
+
 //TestFucntion
-var node_detail;
-node_detail = {node_id: 1, seq_no: 1, send_time: 78343534};
-saveSendTime(node_detail);
-console.log(send_table);
-console.log("\n");
-node_detail = {node_id: 2, seq_no: 1, send_time: 78343534};
-saveSendTime(node_detail);
-console.log(send_table);
-console.log("\n");
-node_detail = {node_id: 2, seq_no: 1, send_time: 7834};
-saveSendTime(node_detail);
-console.log(send_table);
-console.log("\n");
-node_detail = {node_id: 3, seq_no: 1, send_time: 783454};
-saveSendTime(node_detail);
-console.log(send_table);
-console.log("\n");
-
-node_detail = {node_id:2, seq_no:1, send_time:4545};
-var sendTime;
-sendTime = lookupSendTime(node_detail);
-console.log("sendTime = " + sendTime);
-console.log("\n");
-console.log(send_table);
-console.log("\n");
-
-
 // sub saveSendTime {
 // 		 # save nodenr,packetnr,time
 // 		 $nodenr= $_[0]; $packetnr= $_[1]; $time = $_[2];
@@ -85,4 +100,51 @@ console.log("\n");
 // 		}
 // 		return(-1);
 // 	}
+
+// sub networklatency {
+//   # set the output  (result) file name 
+//   $resultlog="network_latency.log"; 
+//   open ($fh_resultlog,">>",$resultlog) or die $!; # open log file for the result
+//   open ($fh_logfile,$logfile) or die $!; # open the log file (from cooja)to be processed for latency
+
+//   # General Idea: when we send the DATA we save node, seqno/packetnr, time to a table 'send'
+//     # when we recv DATA we simply lookup send table and find the sendTime and then computes latency
+//     # and delete that entry from send table, at the end printing send table gives lost packets
+    
+//   # 534188412:4:DATA send to 1 'Hello 8'            
+//   # 534706491:1:DATA recv from 4 'Hello 8'
+//   foreach $line(<$fh_logfile>){ 
+//     if ($line =~ m/(\d+):(\d+):DATA send to 1 'Hello (\d+)'/)  { # line can be either sending or receiving
+//       $nodenr=$2; $packetnr=$3; $time = $1; # save nodenr,packetnr,time 
+//       $noSendPackets = $noSendPackets + 1;  
+//       # printf "DATA send line:$line\n";
+//       saveSendTime($nodenr, $packetnr, $time ); # save this sending time of each packet to the hash %send
+//     } else { # line can be either sending or receiving
+//       if ($line=~ m/(\d+):\d+:DATA recv from (\d+) 'Hello (\d+)'/) {
+//         $nodenr=$2; $packetnr=$3; $time = $1; # save nodenr,packetnr,time   
+//         #printf "DATA recv line:$line\n";
+//         # check if send table has a corresponding sendTime, if yes then calculate latency
+//         $sendTime = lookupSendTime($nodenr, $packetnr,$time);
+//         if ( $sendTime > -1) { # we have a match in sendTable
+// #         # printf "latency for node:$nodenr, packet:$packetnr = %d\n", $time - $sendTime;
+//           $counter = $counter + 1;
+//           $totalLatency = $totalLatency + ($time - $sendTime);
+//         }
+//       } 
+//     }
+//   } # end of foreach
+  
+//   # print loss packets
+// # printf "lost packets are:\n";
+//   $lostpackets = printLostPackets();
+//   printf "NETWORK LATENCY\n";
+//   printf "=================\n";
+//   $row = sprintf  "Average Latency(us)\tno of SendPackets\tLost Packets\n %-22d %-23d %-15d\n" ,$totalLatency / $counter, $noSendPackets, $lostpackets;
+//   printf $row; 
+//   $row = sprintf  "%-d,%-d\r\n" ,$totalLatency / $counter, $lostpackets;
+//   print $fh_resultlog  $row;  
+//   close $fh_resultlog; close $fh_logfile;
+//   system("cp","$resultlog","$resultdir");
+// }
+
 
